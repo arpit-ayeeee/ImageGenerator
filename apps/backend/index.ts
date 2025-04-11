18f9f8e4f85f7680e73dcff97ed235540c17b63e
Arpit Jaiswal <arpitjaiswal282000@gmail.com>
2025-04-11 21:28:10 +0530

    Initialised a backend, with basic express and boilerplate routes

---
+++ apps/backend/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import aiRoutes from './routes/aiRoutes';
+import packRoutes from './routes/packRoutes';
+import imageRoutes from './routes/imageRoutes';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/ai", aiRoutes);
+app.use("/pack", packRoutes);
+app.use("/image", imageRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ apps/backend/routes/packRoutes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Route to generate a bunch of images based on pack chosen
+router.post("/generate", (req, res) => {
+});
+
+// Route to get all the packs available
+router.get("/bulk", (req, res) => {
+});
+
+export default router; 
